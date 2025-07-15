40982212e26a4179053d896f9e34c0f0cb7e7e9e
Namthi Nguyen <nguyen_dahnny@yahoo.com>
2025-07-14 22:22:51 -0400

    Notes

--- nameformating.py
+++ nameformating.py
@@ -1,4 +1,6 @@
 def format_name(first_name, last_name):
+    #.capitalize() function capitalize the first letter 
+    #using [0].upper() only give you back the first inital as a capitalize which return only those char
     first_name_fix = first_name.capitalize()
     last_name_fix = last_name.capitalize()
     full_name_fix = first_name_fix + " " +last_name_fix
